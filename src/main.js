98b15611baabc233075e7d8473823709c5a8170d
utkarsh-supradigit <168803333+utkarsh-supradigit@users.noreply.github.com>
2024-07-31 12:41:11 +0530

    updated changes

--- src/main.js
+++ src/main.js
@@ -1,19 +1,13 @@
 import './style.css'
 
-// document.querySelector('#app').innerHTML = `
-//   <div>
-//     <a href="https://vitejs.dev" target="_blank">
-//       <img src="${viteLogo}" class="logo" alt="Vite logo" />
-//     </a>
-//     <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-//       <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-//     </a>
-//     <h1>Hello Vite!</h1>
-//     <div class="card">
-//       <button id="counter" type="button"></button>
-//     </div>
-//     <p class="read-the-docs">
-//       Click on the Vite logo to learn more
-//     </p>
-//   </div>
-// `
+const mobileMenuIcon = document.querySelector(".hamburger-menu");
+const mobileNav = document.querySelector(".mobile-menu");
+const mobileMenuCloseIcon = document.querySelector(".close")
+
+mobileMenuCloseIcon.addEventListener("click", event => {
+    mobileNav.style.transform = "translateX(100%)";
+})
+
+mobileMenuIcon.addEventListener("click", event => {
+    mobileNav.style.transform = "translateX(0)";
+})
